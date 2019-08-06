3a8f0e1b31e2fa4c683b1307761214f36843eac5
Denis Samborsky <dennyjuice@gmail.com>
2019-08-06 21:27:49 +0700

    add emoji

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from glob import glob
 import logging
 from random import choice
+from emoji import emojize
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -10,21 +11,30 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
 
-def greet_user(bot, update):
-    text = 'Че нада, пля?'
-    logging.info(text)
+def greet_user(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_data['emo'] = emo
+    text = 'Че нада, пля? {}'.format(emo)
     update.message.reply_text(text)
 
-def talk_to_me(bot, update):
-    user_text = "{}! Cам {}...".format(update.message.chat.first_name, update.message.text)
+def talk_to_me(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_text = "{}! Cам {}... {}".format(update.message.chat.first_name, update.message.text, user_data['emo'])
     logging.info("User: %s, Chat ID: %s, Message: %s", update.message.chat.username,
                 update.message.chat.id, update.message.text)
     update.message.reply_text(user_text)
 
-def send_cat_picture(bot, update):
+def send_cat_picture(bot, update, user_data):
     cat_list = glob('images/cat*.jp*g')
     cat_pic = choice(cat_list)
     bot.send_photo(chat_id = update.message.chat_id, photo = open(cat_pic, 'rb'))
+
+def get_user_emo(user_data):
+    if 'emo' in user_data:
+        return user_data['emo']
+    else:
+        user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
+        return user_data['emo']
 
 def main():
     mybot = Updater(settings.API_KEY)
@@ -32,9 +42,9 @@ def main():
     logging.info("I'm back")
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler('start', greet_user))
-    dp.add_handler(CommandHandler('cat', send_cat_picture))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler('start', greet_user, pass_user_data=True))
+    dp.add_handler(CommandHandler('cat', send_cat_picture, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
 
     mybot.start_polling()
     mybot.idle()
